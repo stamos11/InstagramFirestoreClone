3f1fb4032b16321501a47e0fe91283a0bf2745f0
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-03-08 20:54:50 +0200

    Setting Collection View

--- InstagramFirestoreClone/Controller/FeedController.swift
+++ InstagramFirestoreClone/Controller/FeedController.swift
@@ -7,10 +7,44 @@
 
 import UIKit
 
-class FeedController: UIViewController {
+class FeedController: UICollectionViewController {
+
+private let reuseIdentifier = "Cell"
+// MARK: -Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
         
-        view.backgroundColor = .systemBlue
+    }
+    
+    
+    // MARK: -Helpers
+    
+    func configureUI() {
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+
+// MARK: -UIcollectionViewDataSource
+extension FeedController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// MARK: -UICollectionViewDataSource
+
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
     }
 }
--- InstagramFirestoreClone/Controller/MainTabController.swift
+++ InstagramFirestoreClone/Controller/MainTabController.swift
@@ -24,8 +24,8 @@ class MainTabController: UITabBarController {
         view.backgroundColor = .white
         tabBar.backgroundColor = .white
         tabBar.tintColor = .black
-        
-        let feed = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "home_unselected"), selectedImage: UIImage(imageLiteralResourceName: "home_selected"), rootViewController: FeedController())
+        let layout = UICollectionViewFlowLayout()
+        let feed = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "home_unselected"), selectedImage: UIImage(imageLiteralResourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout))
         let search = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "search_unselected"), selectedImage: UIImage(imageLiteralResourceName: "search_selected"), rootViewController: SearchController())
         let imageSelector = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "plus_unselected"), selectedImage: UIImage(imageLiteralResourceName: "plus_unselected"), rootViewController: ImageSelectorController())
         let notification = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "like_unselected"), selectedImage: UIImage(imageLiteralResourceName: "like_selected"), rootViewController: NotificationsController())
