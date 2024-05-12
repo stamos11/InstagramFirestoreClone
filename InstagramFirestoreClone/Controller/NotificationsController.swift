85a4e08a49fc5c1261f00f5a68d50486db90657f
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-05-12 23:38:11 +0300

    Fetching Notifications

--- InstagramFirestoreClone/Controller/NotificationsController.swift
+++ InstagramFirestoreClone/Controller/NotificationsController.swift
@@ -11,15 +11,26 @@ private let reuseIdentifier = "NotificationCell"
 class NotificationsController: UITableViewController {
     
     //MARK: -Properties
-    
+    private var notifications = [Notification]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     
     //MARK: -Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        fetchNotifications()
     }
-    
+    //MARK: -API
+    func fetchNotifications() {
+        NotificationService.fetchNotifications { notifications in
+            self.notifications = notifications
+            print("DEBUG: Notifications \(notifications)")
+        }
+    }
     //MARK: -Helpers
     
     func configureTableView() {
@@ -35,7 +46,7 @@ class NotificationsController: UITableViewController {
 }
 extension NotificationsController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return notifications.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
