6e7013881efd9c6a28baf727089038bd97076be1
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-05-12 22:41:35 +0300

    Notification Data Model

---
+++ InstagramFirestoreClone/Model/Notification.swift
@@ -0,0 +1,40 @@
+//
+//  Notification.swift
+//  InstagramFirestoreClone
+//
+//  Created by stamoulis nikolaos on 12/5/24.
+//
+
+import Firebase
+
+enum NotificationType: Int {
+    case like
+    case follow
+    case comment
+    
+    var notificationMessage: String {
+        switch self {
+        case .like: return " liked your post."
+        case .follow: return " started following you"
+        case .comment: return " commented on your post"
+        }
+    }
+}
+struct Notification {
+    
+    let uid: String
+    let postImageUrl: String?
+    var postId: String?
+    let timestamp: Timestamp
+    let type: NotificationType
+    let id: String
+    
+    init(dictionary: [String: Any]) {
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+        self.id = dictionary["id"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.postId = dictionary["postId"] as? String ?? ""
+        self.postImageUrl = dictionary["postImageUrl"] as? String ?? ""
+        self.type = NotificationType(rawValue: dictionary["type"] as? Int ?? 0) ?? .like
+    }
+}
